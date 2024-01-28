50fe7d0e7fb1a0a9abc91b93d37a40fff013b1aa
Nathan-Salin <nathan.salin@orange.fr>
2024-01-28 19:23:17 +0100

    Adding clap to last scene

--- Global-Game-Jam-Fou-Allie/Assets/Script/SceneSwapper.cs
+++ Global-Game-Jam-Fou-Allie/Assets/Script/SceneSwapper.cs
@@ -5,6 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class SceneSwapper : MonoBehaviour
 {
+    private AudioManager audioManager;
+    public int nb_of_clap_to_hear = 4;
+
+    void Start()
+    {
+        audioManager = GameObject.Find("AudioManager").GetComponent<AudioManager>();
+        for (int i = 0; i < nb_of_clap_to_hear; i++)
+        {
+            audioManager.PlayRandomClap();
+        }
+    }
 
     public static void Go_back_to_main_menu()
     {
