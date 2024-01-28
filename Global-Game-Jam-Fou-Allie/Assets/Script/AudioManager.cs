ea16f56d2ca710454a59171d56c79f6e8e8ba52d
Maxence Salin <maxence.salin@etu.u-bordeaux.fr>
2024-01-28 14:03:01 +0100

    Fix sound issue

--- Global-Game-Jam-Fou-Allie/Assets/Script/AudioManager.cs
+++ Global-Game-Jam-Fou-Allie/Assets/Script/AudioManager.cs
@@ -10,14 +10,7 @@ public class AudioManager : MonoBehaviour
     // Start is called before the first frame update
     void Awake()
     {
-        if(instance ==null){
-            instance=this;
-        }
-        else{
-            Destroy(gameObject);
-            return;
-        }
-        DontDestroyOnLoad(gameObject);
+
         foreach (Sound s in sounds){
             s.source=gameObject.AddComponent<AudioSource>();
             s.source.clip=s.clip;
@@ -28,7 +21,7 @@ public class AudioManager : MonoBehaviour
     }
 
     void Start(){
-        Play("Test");
+        Play("BackgroundMusic");
     }
     public void Play (string name){
         Sound s=Array.Find(sounds, sound => sound.name == name);
